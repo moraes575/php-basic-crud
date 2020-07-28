8cd8728d7aeacbe0222bc737fc62498cf46ffb24
Matheus Moraes <matheus.moraes111@outlook.com>
2020-07-28 16:45:47 -0300

    Success message added

--- process.php
+++ process.php
@@ -1,5 +1,6 @@
 <?php
 
+session_start();
 include_once("connection.php");
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
@@ -10,3 +11,11 @@ $insert_employee = "INSERT INTO employees (`name`, `birth_date`, `occupation`) V
 ('$name', '$birth_date', '$occupation')";
 
 $execute_query = mysqli_query($connection, $insert_employee);
+
+if (mysqli_insert_id($connection)) {
+    $_SESSION['msg'] = "<p>Funcionário cadastrado com sucesso!</p>";
+    header("Location: view/index.php");
+} else {
+    $_SESSION['msg'] = "<p>Erro ao cadastrar funcionário!</p>";
+    header("Location: view/index.php");
+}
